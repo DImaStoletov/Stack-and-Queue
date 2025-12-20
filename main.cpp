4daaa18bd287aeae1867a5ae610e4eae383d8bd9
DimaStoletov <st130013@student.spbu.ru>
2025-12-20 14:39:29 +0300

    Last

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include "Stack.h"
 #include "Queue.h"
-//#include "gtest/gtest.h"
+#include "gtest/gtest.h"
 using namespace std;
 
 
-int main(void){
-    return 0;
-}
+//int main(void){
+//    return 0;
+//}
